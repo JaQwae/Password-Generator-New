d938e841ba5925166e53a8a1434e4343384fccdf
JaQwae <jaqwaeellison@gmail.com>
2024-12-15 14:18:00 -0800

    checked boxes for password settings are return true and false based on if they are clicked or not

--- javascrpit-files/index.js
+++ javascrpit-files/index.js
@@ -2,9 +2,18 @@
 const generatePassword = () => {
     alert(`Generation Started`);
     alert(`Length is ${getPasswordLength()}`);
+    alert(`Password uses lower: ${lowercaseInput.checked}`);
+    alert(`Password uses upper: ${uppercaseInput.checked}`);
+    alert(`Password uses #s: ${numbersInput.checked}`);
+    alert(`Password uses symbols: ${symbolsInput.checked}`);
 }
 
 const getPasswordLength = () => {
     return document.querySelector("#pass-length-input").value;
 }
 
+// Input field for password settings
+const lowercaseInput = document.querySelector("#lowercase");
+const uppercaseInput = document.querySelector("#uppercase");
+const numbersInput = document.querySelector("#numbers");
+const symbolsInput = document.querySelector("#symbols");
