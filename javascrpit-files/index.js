75e654e403a1919f3c66169c8944b1a6a813aba0
JaQwae <jaqwaeellison@gmail.com>
2024-12-17 23:05:44 -0800

    lowercase letter setting is randomizing and displaying

--- javascrpit-files/index.js
+++ javascrpit-files/index.js
@@ -1,19 +1,42 @@
-
 const generatePassword = () => {
-    alert(`Generation Started`);
-    alert(`Length is ${getPasswordLength()}`);
-    alert(`Password uses lower: ${lowercaseInput.checked}`);
-    alert(`Password uses upper: ${uppercaseInput.checked}`);
-    alert(`Password uses #s: ${numbersInput.checked}`);
-    alert(`Password uses symbols: ${symbolsInput.checked}`);
+    passwordRandomizer(getPasswordLength(), lowercaseInput.checked);
+    displayPassword();
 }
+
 
 const getPasswordLength = () => {
     return document.querySelector("#pass-length-input").value;
 }
+
+
+const passwordRandomizer = (length, lower) => {
+    let char = "";
+    for (let i=0; i<length; i++) {
+        if (lower) {
+            char=getLowercaseChar();
+        }
+        password.push(char);
+    }
+}
+
+
+// Generates a random lower case letter
+const getLowercaseChar = () => {
+    let randomInt = Math.floor(Math.random() * (122-97) + 97);
+    return String.fromCharCode(randomInt);
+}
+
+
+const displayPassword = () => {
+    document.getElementById("generated-password").innerHTML = password.join(" ").replaceAll(" ", "") ;
+}
+
 
 // Input field for password settings
 const lowercaseInput = document.querySelector("#lowercase");
 const uppercaseInput = document.querySelector("#uppercase");
 const numbersInput = document.querySelector("#numbers");
 const symbolsInput = document.querySelector("#symbols");
+
+// Password placeholder
+let password=[];
