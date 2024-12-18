9dfb98bcb904d3de5d249159db927e3f900a809a
JaQwae <jaqwaeellison@gmail.com>
2024-12-18 14:00:39 -0800

    uppercase character are showing up in the password

--- javascrpit-files/index.js
+++ javascrpit-files/index.js
@@ -1,5 +1,11 @@
 const generatePassword = () => {
-    passwordRandomizer(getPasswordLength(), lowercaseInput.checked);
+    let passwordSettings = getPasswordSettings(
+        lowercaseInput, 
+        uppercaseInput, 
+        numbersInput, 
+        symbolsInput
+    )
+    passwordRandomizer(getPasswordLength(), passwordSettings);
     displayPassword();
 }
 
@@ -8,27 +14,63 @@ const getPasswordLength = () => {
     return document.querySelector("#pass-length-input").value;
 }
 
+// Return the password setting preferences the user selected
+const getPasswordSettings = (...args) => {
+    const settings = [];
 
-const passwordRandomizer = (length, lower) => {
-    let char = "";
-    for (let i=0; i<length; i++) {
-        if (lower) {
-            char=getLowercaseChar();
+    for(let arg in args) {
+        let inputField = args[arg];
+
+        if(inputField.checked) {
+            settings.push(inputField.id);
         }
+    }
+
+    return settings;
+}
+
+const passwordRandomizer = (...settings) => {
+    const passwordLength = settings[0];
+    const selectedSettings = settings[1];
+    const passSetArrLen = selectedSettings.length
+
+    for (let i=0; i<passwordLength; i++) {
+        let char = randomSettingSelector(selectedSettings[Math.floor(Math.random() * passSetArrLen)])
         password.push(char);
+    }
+}
+
+// Handles which character generator will be call
+const randomSettingSelector = (setting) => {
+    switch (setting) {
+        case 'lowercase':
+            return getLowercaseChar();
+        case 'uppercase':
+            return getUppercaseChar();
+        default:
+            "something went wrong"
     }
 }
 
 
 // Generates a random lower case letter
 const getLowercaseChar = () => {
-    let randomInt = Math.floor(Math.random() * (122-97) + 97);
+    let randomInt = Math.floor(Math.random() * (123-97) + 97);
+    return String.fromCharCode(randomInt);
+}
+
+
+// Generates a random upper case letter
+const getUppercaseChar = () => {
+    let randomInt = Math.floor(Math.random() * (91-65) + 65);
     return String.fromCharCode(randomInt);
 }
 
 
 const displayPassword = () => {
-    document.getElementById("generated-password").innerHTML = password.join(" ").replaceAll(" ", "") ;
+    document.getElementById("generated-password")
+        .innerHTML = password
+        .join(" ").replaceAll(" ", "");
 }
 
 
